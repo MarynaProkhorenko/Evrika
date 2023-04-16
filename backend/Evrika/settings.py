c3452edf17d5edc29fafe5f39920cceedd59da29
eugene <bantyc8@gmail.com>
2023-04-16 11:43:32 +0300

    Put SECRET_KEY of PR to .env

--- backend/Evrika/settings.py
+++ backend/Evrika/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-)538j08lcuy6omu*=jp5$k68-o4*zj!yyomcl=wm3tz#-+gfx+"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
